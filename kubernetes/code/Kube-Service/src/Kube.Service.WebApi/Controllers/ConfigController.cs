e16a1120e007a4005c517c5a0953f4a327115c72
greco <blgreco72dev@gmail.com>
2020-10-09 17:17:27 -0400

    Additional notes and examples.

--- kubernetes/code/Kube-Service/src/Kube.Service.WebApi/Controllers/ConfigController.cs
+++ kubernetes/code/Kube-Service/src/Kube.Service.WebApi/Controllers/ConfigController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kube.Service.WebApi
@@ -9,5 +11,18 @@ namespace Kube.Service.WebApi
         [HttpGet("command-args")]
         public IActionResult GetCommandArgs() => Ok(Environment.GetCommandLineArgs());
 
+        [HttpGet("volume")]
+        public async Task<IActionResult> ReadVolumeConfig([FromQuery]string file) 
+        {
+            string filePath = Path.Join("/etc/kube-config/settings", file);
+
+            if (! System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            string content = await System.IO.File.ReadAllTextAsync(filePath);
+            return Ok(content);
+        }
     }
 }
